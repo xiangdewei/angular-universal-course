bf54483832db4d1c0381a9b4d5ad46b3190fddf7
JHades <jhades.dev@gmail.com>
2018-05-11 10:41:10 +0200

    Course updated to Angular 6

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import * as angularUniversal from 'angular-universal-express-firebase';
 
 export let ssrApp = angularUniversal.trigger({
     index: __dirname + '/index-server.html',
-    main: __dirname + '/main.bundle',
+    main: __dirname + '/main',
     enableProdMode: true,
     cdnCacheExpiry: 600,
     browserCacheExpiry: 300,
